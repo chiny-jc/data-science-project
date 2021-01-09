52af90a3adb008d37304fae8d1ad05904c4eceba
Joshua Campos <chiny.jc@gmail.com>
2021-01-09 14:02:47 +0100

    Added code from notebook

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import json
+import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 with open('train.json') as f:
     data = json.load(f)
@@ -7,3 +9,8 @@ with open('train.json') as f:
 df = pd.DataFrame(data)
 
 df.describe()
+
+np.random.seed(123)
+df = df.sample(frac=1) # shuffle data
+df_dev, df_test = train_test_split(df, test_size=0.15)
+df_train, df_valid = train_test_split(df_dev, test_size=0.15)
